d53e89d59b3d67dee0002ccc5354ecbb1c07512a
asherp <apembroke@gmail.com>
2019-12-23 19:20:49 -0600

    making hydra_app relocatable

--- examples/advanced/hydra_app_example/hydra_app/main.py
+++ examples/advanced/hydra_app_example/hydra_app/main.py
@@ -2,10 +2,22 @@
 
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 import hydra
+from omegaconf import OmegaConf
+from os import path
 
 
-@hydra.main(config_path="config.yaml")
+def config_override(cfg):
+    """Checks to see if user-defined configuration exists"""
+    override_path = hydra.utils.to_absolute_path(cfg.config_override)
+    if path.exists(override_path):
+        override_conf = OmegaConf.load(override_path)
+        cfg = OmegaConf.merge(cfg, override_conf)
+    return cfg
+
+
+@hydra.main(config_path="config.yaml", strict = False)
 def main(cfg):
+    cfg = config_override(cfg)
     print(cfg.pretty())
 
 
--- examples/advanced/hydra_app_example/tests/test_example_app.py
+++ examples/advanced/hydra_app_example/tests/test_example_app.py
@@ -11,15 +11,22 @@ import pytest
 from omegaconf import OmegaConf
 
 from hydra.test_utils.test_utils import chdir_hydra_root, does_not_raise
+import os
 
 chdir_hydra_root()
-
+override_file = os.path.dirname(os.path.realpath(__file__)) + '/config.yaml'
 
 def verify_output(result):
     assert OmegaConf.create(str(result.decode("utf-8"))) == OmegaConf.create(
-        dict(dataset=dict(name="imagenet", path="/datasets/imagenet"))
+        dict(config_override="config.yaml",
+            dataset=dict(name="imagenet", path="/datasets/imagenet"))
     )
 
+def verify_override(result):
+    assert OmegaConf.create(str(result.decode("utf-8"))) == OmegaConf.create(
+        dict(config_override=override_file,
+            dataset=dict(name="bespoke", path="/datasets/destiny"))
+    ) 
 
 def test_python_run():
     verify_output(
@@ -33,6 +40,11 @@ def test_installed_run():
     verify_output(subprocess.check_output(["hydra_app"]))
 
 
+def test_installed_run_with_user_override():
+    config_override = 'config_override={}'.format(override_file)
+    verify_override(subprocess.check_output(["hydra_app", config_override]))
+
+
 @pytest.mark.parametrize("env_key", ["HYDRA_MAIN_MODULE", "FB_PAR_MAIN_MODULE"])
 @pytest.mark.parametrize(
     "env_value, expectation",
@@ -40,3 +52,6 @@ def test_installed_run():
 )
 def test_installed_run_with_env_module_override(env_key, env_value, expectation):
     verify_output(subprocess.check_output(["hydra_app"]))
+
+
+
